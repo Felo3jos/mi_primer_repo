324f1863203751c6b977cf9e98a6f634742b684a
FeloTc <felo88trejosc@gmail.com>
2023-05-29 23:26:11 -0600

    mi primer commit

---
+++ Codigo.py
@@ -0,0 +1,8 @@
+nombre = "Felo"
+apellido = " Trejos"
+
+print("Hola " + nombre + apellido + " Bienvenido a Python.")
+
+print("Escoge Numeros")
+numero1 = int(input("Ingresa un numero (1): "))
+numero2 = int(input("Ingresa un numero (2): "))
